dde4691a58ac96dc716b1ba385baf9b10b38a877
fabrice1618 <fabrice1618@gmail.com>
2019-10-17 14:23:50 +0200

    booleen

---
+++ booleen.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$erreur = true;
+
+if ($erreur) {
+  echo "Il y a une erreur";
+} else {
+  echo "Tout va bien";
+}
+
+
+
+$a = true;
+//$a = false;
+if ($a==true) {
+  echo "C'est vrai";
+} else {
+  echo "C'est faux";
+}
