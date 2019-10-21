8b77106172ef4ed91d97e6dc9b496d8f6244567a
fabrice1618 <fabrice1618@gmail.com>
2019-10-21 12:22:19 +0200

    si

---
+++ si.php
@@ -0,0 +1,23 @@
+<?php
+
+$langue = 'fr';
+
+if ($langue=='fr') {
+  echo "C'est en francais\n";
+} elseif ($langue=='en') {
+  echo "C'est en anglais\n";
+} else {
+  echo "Bachibouzouk";
+}
+
+/*
+$a=3;
+
+if (($a==2) || ($a==3)){
+  echo "La variable vaut 2 ou 3\n";
+} elseif (($a==5) || ($a==6)){
+  echo "La variable vaut 5 ou 6\n";
+} else {
+  echo "La variable vaut autre chose\n";
+}
+*/
